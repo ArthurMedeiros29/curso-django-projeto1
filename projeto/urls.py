f3caff5f93a32100203b3bc43e9304e51f8a87cc
Arthur Medeiros de Oliveira <arthurmedeiros193@gmail.com>
2023-03-24 15:54:35 -0300

    Changes urls and views and add urls in recipes

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,25 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-
-
-def home(request):
-    return HttpResponse('HOME')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return HttpResponse('contato')
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # HOME
-    path('sobre/', sobre),  # /sobre/
-    path('contato/', contato),  # /contato/
+    path('', include('recipes.urls')),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
+
+
+def contato(request):
+    return HttpResponse('contato')
 # Create your views here.
