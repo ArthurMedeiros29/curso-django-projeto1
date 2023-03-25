0168923b548c9834b5e2df0ebafbca507ee75308
Arthur Medeiros de Oliveira <arthurmedeiros193@gmail.com>
2023-03-25 13:39:56 -0300

    Remove sobre and contato

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Arthur',
     })
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
 # Create your views here.
