f974f0b7671ed4d12a7ee33837444a3e567cd97f
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-04-23 14:56:11 -0300

    Validação das receitas

--- authors/forms/register_form.py
+++ authors/forms/register_form.py
@@ -1,101 +1,84 @@
+from collections import defaultdict
+
 from django import forms
-from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from utils.django_forms import add_placeholder, strong_password
+from recipes.models import Recipe
+from utils.django_forms import add_attr
+from utils.strings import is_positive_number
 
 
-class RegisterForm(forms.ModelForm):
+class AuthorRecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        add_placeholder(self.fields['username'], 'Your username')
-        add_placeholder(self.fields['email'], 'Your e-mail')
-        add_placeholder(self.fields['first_name'], 'Ex.: John')
-        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
-        add_placeholder(self.fields['password'], 'Type your password')
-        add_placeholder(self.fields['password2'], 'Repeat your password')
 
-    username = forms.CharField(
-        label='Username',
-        help_text=(
-            'Username must have letters, numbers or one of those @.+-_. '
-            'The length should be between 4 and 150 characters.'
-        ),
-        error_messages={
-            'required': 'This field must not be empty',
-            'min_length': 'Username must have at least 4 characters',
-            'max_length': 'Username must have less than 150 characters',
-        },
-        min_length=4, max_length=150,
-    )
-    first_name = forms.CharField(
-        error_messages={'required': 'Write your first name'},
-        label='First name'
-    )
-    last_name = forms.CharField(
-        error_messages={'required': 'Write your last name'},
-        label='Last name'
-    )
-    email = forms.EmailField(
-        error_messages={'required': 'E-mail is required'},
-        label='E-mail',
-        help_text='The e-mail must be valid.',
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(),
-        error_messages={
-            'required': 'Password must not be empty'
-        },
-        help_text=(
-            'Password must have at least one uppercase letter, '
-            'one lowercase letter and one number. The length should be '
-            'at least 8 characters.'
-        ),
-        validators=[strong_password],
-        label='Password'
-    )
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(),
-        label='Password2',
-        error_messages={
-            'required': 'Please, repeat your password'
-        },
-    )
+        self._my_errors = defaultdict(list)
+
+        add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
 
     class Meta:
-        model = User
-        fields = [
-            'first_name',
-            'last_name',
-            'username',
-            'email',
-            'password',
-        ]
+        model = Recipe
+        fields = 'title', 'description', 'preparation_time', \
+            'preparation_time_unit', 'servings', 'servings_unit', \
+            'preparation_steps', 'cover'
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Pedaços', 'Pedaços'),
+                    ('Pessoas', 'Pessoas'),
+                ),
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos', 'Minutos'),
+                    ('Horas', 'Horas'),
+                ),
+            ),
+        }
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email', '')
-        exists = User.objects.filter(email=email).exists()
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        cd = self.cleaned_data
 
-        if exists:
-            raise ValidationError(
-                'User e-mail is already in use', code='invalid',
-            )
+        title = cd.get('title')
+        description = cd.get('description')
 
-        return email
+        if title == description:
+            self._my_errors['title'].append('Cannot be equal to description')
+            self._my_errors['description'].append('Cannot be equal to title')
 
-    def clean(self):
-        cleaned_data = super().clean()
+        if self._my_errors:
+            raise ValidationError(self._my_errors)
 
-        password = cleaned_data.get('password')
-        password2 = cleaned_data.get('password2')
+        return super_clean
 
-        if password != password2:
-            password_confirmation_error = ValidationError(
-                'Password and password2 must be equal',
-                code='invalid'
-            )
-            raise ValidationError({
-                'password': password_confirmation_error,
-                'password2': [
-                    password_confirmation_error,
-                ],
-            })
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+
+        if len(title) < 5:
+            self._my_errors['title'].append('Must have at least 5 chars.')
+
+        return title
+
+    def clean_preparation_time(self):
+        field_name = 'preparation_time'
+        field_value = self.cleaned_data.get(field_name)
+
+        if not is_positive_number(field_value):
+            self._my_errors[field_name].append('Must be a positive number')
+
+        return field_value
+
+    def clean_servings(self):
+        field_name = 'servings'
+        field_value = self.cleaned_data.get(field_name)
+
+        if not is_positive_number(field_value):
+            self._my_errors[field_name].append('Must be a positive number')
+
+        return field_value
