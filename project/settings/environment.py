603dfd885c9454804002c8df96df683d7e24b05f
ArthurMedeiros29 <arthurmedeiros193@gmail.com>
2023-04-29 15:46:20 -0300

    Organizando o arquivo settings.py

---
+++ project/settings/environment.py
@@ -0,0 +1,30 @@
+'''import os'''
+from pathlib import Path
+
+from utils.environment import get_env_variable, parse_comma_sep_str_to_list
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-xl11#if5xm5%b0col9fa57xv+o6z(0wl0*22e2gm)td4mk)q8e'  # noqa: E501
+'''SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')'''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+'''DEBUG = True if os.environ.get('DEBUG') == '1' else False'''
+
+ALLOWED_HOSTS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('ALLOWED_HOST')
+)
+CSRF_TRUSTED_ORIGINS: list[str] = parse_comma_sep_str_to_list(
+    get_env_variable('CSRF_TRUSTED_ORIGINS')
+)
+
+ROOT_URLCONF = 'project.urls'
+
+WSGI_APPLICATION = 'project.wsgi.application'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
